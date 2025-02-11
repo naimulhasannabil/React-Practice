bc70df123002d7241a4320d002ba25c33a7e5d5e
Naimul Hasan Nabil <naimulhasannabil@gmail.com>
2025-02-11 19:08:28 +0600

    Update The react Code

--- Practice/src/App.jsx
+++ Practice/src/App.jsx
@@ -7,15 +7,20 @@ function App() {
   return (
     <>
       <nav>
-        <div className="logo ">
+        <div className='container sm:flex py-2 ml-5 mr-5'>
+        <div className="logo text-3xl">
           TextUtils
         </div>
-        <div className="container">
-            <ul>
-              <li>Home</li>
-              <li>About</li>
-              <li>Contact</li>
+        <div className="container sm:flex justify-between py-2">
+            <ul className='sm:flex'>
+              <li><a className='sm:p-7' href="/">Home</a></li>
+              <li><a href="/">about</a></li>
             </ul>
+            <form action="" className='sm:flex mr-4'>
+              <input className='border' type="search" placeholder='Search' aria-label='Search'/>
+              <button className='p-3 mr-7' type='submit'>Search</button>
+            </form>
+        </div>
         </div>
       </nav>
     </>
