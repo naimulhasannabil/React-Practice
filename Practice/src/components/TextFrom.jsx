bcd9b6e8183175d8fa6eab75465bb797a4dcb506
Naimul Hasan Nabil <naimulhasannabil@gmail.com>
2025-03-01 22:12:12 +0600

    Update The Code

--- Practice/src/components/TextFrom.jsx
+++ Practice/src/components/TextFrom.jsx
@@ -4,12 +4,8 @@ export default function TextFrom() {
   return (
     <div>
       <div className="mb-3">
-        <label htmlFor="exampleFromControlInput1" className='form-label'>Email Address</label>
-        <input type="email" className='form-control' id='exampleFromControlInput1' placeholder='name@example.com'/>
-      </div>
-      <div className="mb-3">
-        <label htmlFor="exampleFromControlTextarea1" className='form-label'>Example textarea</label>
-        <textarea name="" id="exampleFromControlTextarea1" rows='3' className='form-control'></textarea>
+        <label htmlFor="myBox" className='form-label'>Example textarea</label>
+        <textarea name="" id="myBox" rows='3' className='form-control'></textarea>
       </div>
     </div>
   )
