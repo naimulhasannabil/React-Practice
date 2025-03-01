27b06cca44d34e31fff3c79819ef4da6ee07cd0b
Naimul Hasan Nabil <naimulhasannabil@gmail.com>
2025-03-01 23:17:03 +0600

    Update the Code

--- Practice/src/components/TextFrom.jsx
+++ Practice/src/components/TextFrom.jsx
@@ -4,14 +4,16 @@ import React, {useState} from 'react'
 
 export default function TextFrom(props) {
     const [text, setText] = useState('Enter text here');
+    // text = 'new Text'; wrong way to change the state
+    // setText('new Text'); Correct way to change the state
   return (
     <div>
-        <h1 className='ml-3 my-3 text-3xl'>{props.heading}</h1>
+        <h1 className='ml-3 my-3 text-3xl'>{props.heading} - {text}</h1>
       <div className="p-4">
       <textarea
         id="description"
         name="description"
-        rows={10}
+        rows={10} value={text}
         className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
         placeholder="Enter your description here..."
       ></textarea>
