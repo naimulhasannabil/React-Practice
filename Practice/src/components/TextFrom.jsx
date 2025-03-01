1903ec890a2650d6890c5ceedb6ef0d864d148b2
Naimul Hasan Nabil <naimulhasannabil@gmail.com>
2025-03-01 22:04:41 +0600

    Update the code

---
+++ Practice/src/components/TextFrom.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function TextFrom() {
+  return (
+    <div>
+      <div className="mb-3">
+        <label htmlFor="exampleFromControlInput1" className='form-label'>Email Address</label>
+        <input type="email" className='form-control' id='exampleFromControlInput1' placeholder='name@example.com'/>
+      </div>
+      <div className="mb-3">
+        <label htmlFor="exampleFromControlTextarea1" className='form-label'>Example textarea</label>
+      </div>
+    </div>
+  )
+}
