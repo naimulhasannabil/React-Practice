816b53d21980ce1d28ebedeafaf7fc0d7f9ebd58
Naimul Hasan Nabil <naimulhasannabil@gmail.com>
2025-03-02 14:45:26 +0600

    Update The Code

--- Practice/src/components/TextFrom.jsx
+++ Practice/src/components/TextFrom.jsx
@@ -7,6 +7,9 @@ export default function TextFrom(props) {
     const handleUpClick = ()=> {
       console.log("UpperCase was click")
     }
+    const handleOnChange = ()=> {
+      console.log("On Change")
+    }
     const [text, setText] = useState('Enter text here');
     // text = 'new Text'; wrong way to change the state
     // setText('new Text'); Correct way to change the state
@@ -19,7 +22,7 @@ export default function TextFrom(props) {
         name="description"
         rows={10} value={text}
         className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-        placeholder="Enter your description here..."
+        placeholder="Enter your description here..." onChange={handleOnChange}
       ></textarea>
     </div>
     <button className="ml-5 border border-green-600 bg-green-500 hover:bg-green-600 text-white font-semibold rounded-lg py-2 px-4 transition duration-300 ease-in-out transform hover:scale-105 shadow-md" onClick={handleUpClick}>
